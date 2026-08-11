8f3b246389ba89f5623bcbef92377ef4779d6116
teketim <tektyme3033@gmail.com>
2026-08-10 21:07:30 -0500

    feat: establish Project Ascend Terraform foundation

---
+++ Labs/Lab-01-Resource-Group/outputs.tf
@@ -0,0 +1,7 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.project_ascend.name
+}
+
+output "resource_group_location" {
+  value = azurerm_resource_group.project_ascend.location
+}
